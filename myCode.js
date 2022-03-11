57f7d5a42096f874c5d25789418c65beff646005
kwage <kylewegener@gmail.com>
2022-03-11 08:27:35 -0600

    Completed examples for lecture 17

--- myCode.js
+++ myCode.js
@@ -1,3 +1,33 @@
 // WEATHER ENDPOINT: https://handlers.education.launchcode.org/static/weather.json
 // CAT FACTS ENDPOINT: https://cat-fact.herokuapp.com/facts
 
+window.addEventListener('load', async function() {
+
+    let myCatFact = await getCatFact();
+    let myDidYouKnowEle = document.getElementById('animal-fact');
+    myDidYouKnowEle.innerText = myCatFact;
+
+});
+
+async function getCatFact() {
+    let myCatFact = await fetch('https://cat-fact.herokuapp.com/facts')
+    .then(function(response) {
+        return response.json();
+    }).then(function(response) {
+        // response is an array of cat fact objects
+        let randoInt = Math.floor(Math.random() * (response.length));
+        return response[randoInt].text;
+    });
+    return myCatFact;
+}
+
+
+
+    /*await fetch('https://handlers.education.launchcode.org/static/weather.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log('This response has returned!');
+    });
+    console.log('Im here!'); */
